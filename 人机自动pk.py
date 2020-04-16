dd93954279be1597d045c397f8a720f2070b114f
ok863467373 <714260909@qq.com>
2020-04-17 04:07:27 +0800

    更新内部文字显示逻辑 添加询问循环

--- 人机自动pk.py
+++ 人机自动pk.py
@@ -1,57 +1,64 @@
 # -*- coding: utf-8
 
-import time, random
+import time,random
 
-player_victory = 0
-enemy_victory = 0
-i = 0
-while player_victory < 2 and enemy_victory < 2:
-    i += 1
-    print('\n目前比分： %s 比 %s \n' % (player_victory,enemy_victory))
-    time.sleep(2)  # 让局与局之间有较明显的有时间间隔
-    print(' \n——————现在是第' + str(i) + '局——————')  # 作为局的标记
+while True:
+    player_victory = 0
+    enemy_victory = 0
+    i = 0
+    while player_victory < 2 and enemy_victory < 2:
+        i += 1
+        print('\n目前比分： %s 比 %s \n' % (player_victory,enemy_victory))
+        time.sleep(2)  # 让局与局之间有较明显的有时间间隔
+        print(' \n——————现在是第%s局——————' % i)  # 作为局的标记
 
-    player_life = random.randint(100, 150)
-    player_attack = random.randint(30, 50)
-    enemy_life = random.randint(100, 150)
-    enemy_attack = random.randint(30, 50)
+        player_life = random.randint(100, 150)
+        player_attack = random.randint(30, 50)
+        enemy_life = random.randint(100, 150)
+        enemy_attack = random.randint(30, 50)
 
-    # 展示双方角色的属性
-    print('【玩家】\n 血量：%s \n攻击：%s' % (player_life,player_attack))
-    print('------------------------')
-    time.sleep(1)
-    print('【敌人】\n 血量：%s \n攻击：%s' % (enemy_life,enemy_attack))
-    print('------------------------')
-    time.sleep(1)
+        # 展示双方角色的属性
+        print('【玩家】\n 血量：%s \n攻击：%s' % (player_life,player_attack))
+        print('------------------------')
+        time.sleep(1)
+        print('【敌人】\n 血量：%s \n攻击：%s' % (enemy_life,enemy_attack))
+        print('------------------------')
+        time.sleep(1)
 
-    # 双方PK
-    while player_life > 0 and enemy_life > 0:
-        player_life = player_life - enemy_attack
-        enemy_life = enemy_life - player_attack
-        print('你发起了攻击，【敌人】剩余血量' + str(enemy_life))
-        print('敌人向你发起了攻击，【玩家】剩余血量' + str(player_life))
-        print('-----------------------')
-        time.sleep(1.5)
+        # 双方PK
+        while player_life > 0 and enemy_life > 0:
+            player_life = player_life - enemy_attack
+            enemy_life = enemy_life - player_attack
+            print('你发起了攻击，【敌人】剩余血量' + str(enemy_life))
+            print('敌人向你发起了攻击，【玩家】剩余血量' + str(player_life))
+            print('-----------------------')
+            time.sleep(1.5)
 
-    # 打印最终战果
-    if player_life > 0 and enemy_life <= 0:
-        print('敌人死翘翘了，你赢了！')
-        player_victory += 1
-        while player_victory == 2:
-            break
-    elif player_life <= 0 and enemy_life > 0:
-        print('悲催，敌人把你干掉了！')
-        enemy_victory += 1
-        while enemy_victory == 2:
-            break
+        # 打印最终战果
+        if player_life > 0 and enemy_life <= 0:
+            print('敌人死翘翘了，你赢了！')
+            player_victory += 1
+            while player_victory == 2:
+                break
+        elif player_life <= 0 and enemy_life > 0:
+            print('悲催，敌人把你干掉了！')
+            enemy_victory += 1
+            while enemy_victory == 2:
+                break
+        else:
+            print('哎呀，你和敌人同归于尽了！')
+
+    # 判断最终结果
+    if player_life > enemy_life:
+        print('\n玩家胜利\n')
+        print('结果：%s比%s' % (player_victory,enemy_victory))
+
     else:
-        print('哎呀，你和敌人同归于尽了！')
+        print('\n敌人\n')
+        print('结果：%s比%s' % (player_victory,enemy_victory))
 
-# 判断最终结果
-if player_life > enemy_life:
-    print('\n玩家胜利\n')
-    print('结果：%s比%s' % (player_victory,enemy_victory))
-
-else:
-    print('\n敌人\n')
-    print('结果：%s比%s' % (player_victory,enemy_victory))
+    # 在 while True 循环中设置跳出条件。
+    a1 = raw_input('\n要继续游戏吗，请输入n退出，输入其他继续：')
+    if a1 == 'n':
+        print('游戏退出')
+        break
--- 测试.py
+++ 测试.py
@@ -1,13 +1,6 @@
 # -*- coding: utf-8
 
-dct = {'Name': ['Alice','1456'], 'Age':[ 'aa','5aa','345']}
-# 取出字典的值
-for value1 in dct.values():
-   #遍历值列表
-    for value2 in value1:
-        #判断是否为数字的字符串
-        if value2.isdigit():
-            value = value1
-            key = [k for (k,v) in dct.items() if v == value]
-            print("键名:%s" %(str(key)))
-            print("对应的值:%s" %(str(value)))
+import sys
+print('Plase input your name: ')
+name = sys.stdin.readline()
+print('Hello ', name)
